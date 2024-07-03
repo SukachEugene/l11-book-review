686332a31813c2998201deea8b4c867afb80daa2
UJ <sukach.eugene@gmail.com>
2024-07-03 10:50:05 +0300

    provided creating of new review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -18,6 +18,7 @@ class Review extends Model
 
     protected static function booted()
     {
+        Review::created(fn(Review $review) => cache()->forget('book:' . $review->book_id));
         Review::updated(fn(Review $review) => cache()->forget('book:' . $review->book_id));
         Review::deleted(fn(Review $review) => cache()->forget('book:' . $review->book_id));
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Book;
 
@@ -8,4 +9,9 @@ Route::get('/', function () {
     return redirect()->to('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index','show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create','store']);
